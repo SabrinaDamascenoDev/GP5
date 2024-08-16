c82086ead199fba87dd4dfa2e65436fc4d3bbb05
SabrinaDamascenoDev <sabrinadamascenodev@gmail.com>
2024-08-16 20:24:40 -0300

    desencvolvimento do menu da aplicacao

--- gp5/src/App.js
+++ gp5/src/App.js
@@ -1,9 +1,9 @@
-
+import Inicio from "./Componentes/InicioHome/InicioHome";
 
 function App() {
   return (
     <div className="App">
-      
+      <Inicio />
     </div>
   );
 }
--- gp5/src/Componentes/InicioHome/InicioHome.js
+++ gp5/src/Componentes/InicioHome/InicioHome.js
@@ -1,11 +1,30 @@
 
 import 'bootstrap'
+import 'bootstrap-icons/font/bootstrap-icons.css';
 import './inicio.css'
+
 
 function Inicio() {
     return (
       <div className="container">
-          
+          <header>
+              <nav className="navbar navbar-expand-sm mt-2 fs-1">
+                  <h1 className='navbar-brand fw-bold'>GP5.</h1>
+                  <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#menuNavBar">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="collapse navbar-collapse" id="menuNavBar">
+                  <div class="navbar-nav fs-5 ms-5 g-5 aling-items-center mx-auto d-flex justify-content-center">
+                      <a href="#" class="nav-link active">Sobre</a>
+                      <a href="#" class="nav-link">Objetivo</a>
+                      <a href="#" class="nav-link">Equipe</a>
+                      <a href="#" class="nav-link">Acessar</a>
+                  </div>
+                </div>
+
+              </nav>
+          </header>
       </div>
     );
   }
